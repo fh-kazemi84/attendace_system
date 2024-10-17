a6a585d522da12f620e09eafce68e1b3c85504bf
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 11:06:51 +0200

    Add a mthod to show friendly Status

--- attendance_system_backend/Models/Enums/AttendanceStatus.cs
+++ attendance_system_backend/Models/Enums/AttendanceStatus.cs
@@ -10,4 +10,21 @@ namespace attendance_system_backend.Models.Enums
         Late,
         EarlyLeave
     }
+
+    public static class AttendanceStatusExtensions
+    {
+        public static string ToFriendlyString(this AttendanceStatus status)
+        {
+            return status switch
+            {
+                AttendanceStatus.Present => "Present",
+                AttendanceStatus.Absent => "Absent",
+                AttendanceStatus.Leave => "On Leave",
+                AttendanceStatus.HalfDay => "Half Day",
+                AttendanceStatus.Late => "Late",
+                AttendanceStatus.EarlyLeave => "Early Leave",
+                _ => throw new ArgumentOutOfRangeException(nameof(status), status, null)
+            };
+        }
+    }
 }
