177c0d69eb715e32107d3319e8b3b1f7792b1bd9
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 12:21:05 +0200

    Update database configuration

--- attendance_system_backend/Program.cs
+++ attendance_system_backend/Program.cs
@@ -1,6 +1,16 @@
+using attendance_system_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+//Config Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("local");
+    options.UseSqlServer(connectionString);
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
