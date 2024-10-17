92aae611ad24507690a0e2ca6df366dc4b0c2ed3
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 10:43:53 +0200

    Create Department class

---
+++ attendance_system_backend/Models/Department.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace attendance_system_backend.Models
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Department name is required.")]
+        [StringLength(100, ErrorMessage = "Department name can't be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        [StringLength(250, ErrorMessage = "Description can't be longer than 250 characters.")]
+        public string Description { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+
+        public Department()
+        {
+            Employees = new List<Employee>(); // Initialize collection to avoid null references
+        }
+    }
+}
