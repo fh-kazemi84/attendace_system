bcdb6c2d28e4aa11d25fe33c2b22e7f884d6575c
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 10:32:23 +0200

    Install Entity Framework Core

--- attendance_system_backend/obj/Debug/net8.0/attendance_system_backend.AssemblyInfo.cs
+++ attendance_system_backend/obj/Debug/net8.0/attendance_system_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("attendance_system_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7879d72a5313d3dd50f4cb1210955b66e1be6a4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f22fd1f0ed2369418b9f47a1ce5eab49c9e1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("attendance_system_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("attendance_system_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
