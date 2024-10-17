e98afef7eeb2fce3ebe609a161495eef1a1cf7f8
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 11:07:31 +0200

    Add a mthod to show user friendly UserRole

--- attendance_system_backend/Models/Enums/UserRole.cs
+++ attendance_system_backend/Models/Enums/UserRole.cs
@@ -6,4 +6,18 @@ namespace attendance_system_backend.Models.Enums
         Admin, 
         Employee
     }
+
+     public static class UserRoleExtensions
+    {
+        public static string ToFriendlyString(this UserRole userRole)
+        {
+            return userRole switch
+            {
+                UserRole.Manager => "Manager",
+                UserRole.Admin=> "Admin",
+                UserRole.Employee => "Employee",
+                _ => throw new ArgumentOutOfRangeException(nameof(userRole), userRole, null)
+            };
+        }
+    }
 }
