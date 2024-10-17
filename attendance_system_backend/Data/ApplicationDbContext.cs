f1bdf151cac792b9b577348bf33f8b14c03d0d40
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 12:06:38 +0200

    Add application database conntext

---
+++ attendance_system_backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+
+using attendance_system_backend.Models;
+using attendance_system_backend.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace attendance_system_backend.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        // DbSets for each model
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+    }
+}
