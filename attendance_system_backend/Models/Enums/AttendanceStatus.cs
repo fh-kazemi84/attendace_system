b8475108cafafe4bfc60b49b116b06e9bdf8d5a6
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-17 10:52:29 +0200

    Add AttendanceStatus enum

---
+++ attendance_system_backend/Models/Enums/AttendanceStatus.cs
@@ -0,0 +1,13 @@
+namespace attendance_system_backend.Models.Enums
+{
+    public enum AttendanceStatus
+    {
+        
+        Present,
+        Absent,
+        Leave,
+        HalfDay,
+        Late,
+        EarlyLeave
+    }
+}
