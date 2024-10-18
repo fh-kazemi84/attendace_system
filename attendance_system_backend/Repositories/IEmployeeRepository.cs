d79a595f420f3cdc1b9cf6ba521adc1bd56ba55c
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-18 10:03:04 +0200

    Create employee inteface

---
+++ attendance_system_backend/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+
+
+
+using attendance_system_backend.Models;
+
+namespace attendance_system_backend.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllEmployeeAsync();
+        Task<Employee> GetEmployeeByIdAsync(int id);
+        Task AddEmployeeAsync(Employee employee);
+        Task UpdateEmployeeAsync(Employee employee);
+        Task DeleteEmployeeAsync(int id);
+    }
+}
