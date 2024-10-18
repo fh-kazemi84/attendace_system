d3990b537467539143a4c8b747a7dd5887b2a506
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-18 15:31:33 +0200

    Update database configuration

--- attendance_system_backend/Program.cs
+++ attendance_system_backend/Program.cs
@@ -1,5 +1,7 @@
 using attendance_system_backend.Data;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("local");
-    options.UseSqlServer(connectionString);
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
 // Add services to the container.
