e92774122328e480dea3c1a23982fac644b6459f
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-24 16:20:29 +0200

    Create new Interface for department

---
+++ attendance_system_backend/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+using attendance_system_backend.Models;
+
+namespace attendance_system_backend.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetAllDepartmentsAsync();
+        Task<Department> GetDepartmentByIdAsync(int id);
+        Task<Department> AddDepartmentAsync(Department department);
+        Task<Department> UpdateDepartmentAsync(Department department);
+        Task<bool> DeleteDepartmentAsync(int id);
+    }
+}
