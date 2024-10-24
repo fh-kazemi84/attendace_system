8f2743c9b6f9b4cec8392b75a269b34c114c41d7
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-24 16:36:08 +0200

    Update properties and configurations

--- attendance_system_backend/Program.cs
+++ attendance_system_backend/Program.cs
@@ -1,11 +1,21 @@
+using System.Text.Json;
 using attendance_system_backend.Data;
+using attendance_system_backend.Repositories;
+using attendance_system_backend.Repositories.Imp;
+using attendance_system_backend.Services;
+using attendance_system_backend.Services.Imp;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System.Text.Json.Serialization;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(option =>
+    {
+        option.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        option.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
 
 //Config Database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -13,6 +23,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     var connectionString = builder.Configuration.GetConnectionString("local");
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
+
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
