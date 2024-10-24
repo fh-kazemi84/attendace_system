b4f4c03e6fdb92d5b9c41878e5f797df35c9da9f
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-24 15:44:49 +0200

    Update name of class

---
+++ attendance_system_backend/Models/UserInfo.cs
@@ -0,0 +1,24 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using attendance_system_backend.Models.Enums;
+
+namespace attendance_system_backend.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string PasswordHash { get; set; }
+
+        [Required(ErrorMessage = "User role is required.")]
+        public UserRole Role { get; set; }
+
+    }
+}
