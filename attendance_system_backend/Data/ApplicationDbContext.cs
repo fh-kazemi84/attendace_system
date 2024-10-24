d77a107e4d8bbdf2858c7c20f1b6aea0d6ab44e8
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-10-24 16:04:21 +0200

    Update DbSets and create ModelBuilder

--- attendance_system_backend/Data/ApplicationDbContext.cs
+++ attendance_system_backend/Data/ApplicationDbContext.cs
@@ -12,7 +12,27 @@ namespace attendance_system_backend.Data
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Department> Departments { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+        public DbSet<UserInfo> UserInfos { get; set; }  
+        public DbSet<AttendanceRecord> AttendanceRecords { get; set; }  
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Address)
+                .WithOne() 
+                .HasForeignKey<Employee>(e => e.AddressId);
+
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.UserInfo)
+                .WithOne()
+                .HasForeignKey<Employee>(e => e.UserInfoId);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.AttendanceRecords)
+                .WithOne();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
