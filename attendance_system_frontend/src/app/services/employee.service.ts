7ae926587b5dfc1912893e49e71d7c057a5be48b
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-12-18 14:41:28 +0100

    Create and Update EmployeeServices

---
+++ attendance_system_frontend/src/app/services/employee.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+
+import { Employee, Address, UserInfo } from '../models/app-models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  defaultAddress: Address = {
+    id: 0,
+    street: '',
+    city: '',
+    state: '',
+    postalCode: '',
+    country: ''
+  };
+
+  defaultUserInfo: UserInfo = {
+    id: 0,
+    username: '',
+    passwordHash: '',
+    role: -1
+  };
+
+  defaultEmployee: Employee = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    hireDate: new Date(),
+    position: -1,
+    salary: 0,
+    gender: -1,
+    departmentId: -1,
+    addressDTO: { ...this.defaultAddress },
+    userInfoDTO: { ...this.defaultUserInfo },
+    attendanceRecordDTOs: []
+  };
+
+  newEmployee: Employee = { ...this.defaultEmployee };
+
+  private apiUrl = "https://localhost:7279/api/Employee";
+
+  constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiUrl}/employees`);
+  }
+
+  public getEmployeeById(id: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiUrl}/employees/${id}`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiUrl}/add`, employee);
+  }
+
+  public deleteEmployee(id: number): Observable<Employee> {
+    return this.http.delete<Employee>(`${this.apiUrl}/delete/${id}` , {responseType: 'text' as 'json'});
+  }
+}
