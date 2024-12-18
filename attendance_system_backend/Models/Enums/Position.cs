45f97198b860eb1c24db6d7d3824c70df26cc158
fh-kazemi84 <fh.kazemi84@gmail.com>
2024-12-18 14:12:26 +0100

    Update files

---
+++ attendance_system_backend/Models/Enums/Position.cs
@@ -0,0 +1,25 @@
+﻿namespace attendance_system_backend.Models.Enums
+{
+    public enum Position
+    {
+        Manager,
+        Senior,
+        Junior,
+        Office_worker
+    }
+
+    public static class PositionExtensions
+    {
+        public static string ToFriendlyString(this Position position)
+        {
+            return position switch
+            {
+                Position.Manager => "Manager",
+                Position.Senior => "Senior",
+                Position.Junior => "Junior",
+                Position.Office_worker => "Office Worker",
+                _ => throw new ArgumentOutOfRangeException(nameof(position), position, null)
+            } ;
+        }
+    }
+}
