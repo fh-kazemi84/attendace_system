7d1e9d8cdac9f1f5db15a2ea70a50a7abd702a93
fh-kazemi84 <fh.kazemi84@gmail.com>
2025-01-07 11:09:51 +0100

    Add GetAttendanceRecordByAttendancerecordId method

--- attendance_system_backend/Controllers/EmployeeController.cs
+++ attendance_system_backend/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using attendance_system_backend.DTOs;
+using attendance_system_backend.Models;
 using attendance_system_backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,7 +26,7 @@ namespace attendance_system_backend.Controllers
                 var employees = await _employeeService.GetAllEmployeesAsync();
                 return Ok(employees);
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
@@ -39,7 +40,7 @@ namespace attendance_system_backend.Controllers
             {
                 Response.Headers.Add("Access-Control-Allow-Origin", "*");
                 var employee = await _employeeService.GetEmployeeByIdAsync(id);
-                if (employee == null)
+                if(employee == null)
                 {
                     return NotFound();
                 }
@@ -58,16 +59,16 @@ namespace attendance_system_backend.Controllers
         {
             try
             {
-                if (employeeDto == null)
+                if(employeeDto == null)
                 {
                     return BadRequest("Employee object is null.");
                 }
 
                 Response.Headers.Add("Access-Control-Allow-Origin", "*");
                 var createdEmployee = await _employeeService.AddEmployeeAsync(employeeDto);
-                return CreatedAtAction(nameof(GetEmployeeById), new { id = createdEmployee.Id }, createdEmployee);
+                return CreatedAtAction(nameof(GetEmployeeById), new { id = createdEmployee.Id}, createdEmployee);
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
@@ -110,7 +111,7 @@ namespace attendance_system_backend.Controllers
                 }
                 return Ok("Employee deleted successfully. ");
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
@@ -125,6 +126,26 @@ namespace attendance_system_backend.Controllers
                 Response.Headers.Add("Access-Control-Allow-Origin", "*");
                 var attendanceRecords = await _employeeService.GetAttendanceRecordsByEmployeeIdAsync(employeeId);
                 return Ok(attendanceRecords);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        //GET: api/Employee/{employeeId}/{attendanceRecordId}
+        [HttpGet("{employeeId}/{attendanceRecordId}")]
+        public async Task<IActionResult> GetAttendanceRecordByEmployeeIdAndAttendancerecordId(int employeeId, int attendanceRecordId)
+        {
+            try
+            {
+                Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                var attendanceRecord = await _employeeService.GetAttendanceRecordByEmployeeIdAndAttendancerecordIdAsync(employeeId, attendanceRecordId);
+                if(attendanceRecord == null)
+                {
+                    return NotFound();
+                }
+                return Ok(attendanceRecord);
             }
             catch (Exception ex)
             {
