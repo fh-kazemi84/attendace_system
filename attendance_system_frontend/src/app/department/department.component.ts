732a7f83870383701756137892799b7f59c9effe
fh-kazemi84 <fh.kazemi84@gmail.com>
2025-01-14 11:21:41 +0100

    Get departmentlist from DB

--- attendance_system_frontend/src/app/department/department.component.ts
+++ attendance_system_frontend/src/app/department/department.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Department } from '../models/app-models';
+import { Router } from '@angular/router';
+import { DepartmentService } from '../services/department.service';
 
 @Component({
   selector: 'app-department',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
   templateUrl: './department.component.html',
   styleUrl: './department.component.css'
 })
-export class DepartmentComponent {
+export class DepartmentComponent implements OnInit {
 
+  departmentList: Department[];
+  selectedDepartment: Department;
+
+  editDepartmentMode: boolean = false;
+
+  constructor(private router: Router,
+    private departmentService: DepartmentService
+  ) { }
+
+  ngOnInit(): void {
+    this.onLoadDepartments();
+  }
+
+  onLoadDepartments() {
+    this.departmentService.getDepartments().subscribe({
+      next: (departments) => {
+        this.departmentList = departments;
+      },
+      error: (err) => {
+        alert('Failed to load Department List!')
+      }
+    });
+  }
+
+  public onEmployeeList() {
+    this.router.navigate(['/admin/employees']);
+  }
+
+  public onEditDepartment(departmentId) { }
+
+  public onDeleteDepartment(departmentId) { }
 }
