910db0154575c01c7968dedfc6a3157d2724c8a8
fh-kazemi84 <fh.kazemi84@gmail.com>
2025-01-14 14:08:13 +0100

    Update getDepartmentById method

--- attendance_system_frontend/src/app/services/department.service.ts
+++ attendance_system_frontend/src/app/services/department.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { Department } from '../models/app-models';
 import { Observable } from 'rxjs';
@@ -7,38 +7,18 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class DepartmentService implements OnInit {
+export class DepartmentService {
 
   private apiUrl = "https://localhost:7279/api/Department";
 
-  departments: Department[];
-
   constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    //this.loadDepartments();
-    this.getDepartments();
-  }
-
-  public loadDepartments(): void {
-    this.getDepartments().subscribe({
-      next: (departments) => {
-        this.departments = departments;
-      },
-      error: (err) => {
-        console.error("Error loading departments:", err);
-      }
-    });
-  }
 
   public getDepartments(): Observable<Department[]> {
     return this.http.get<Department[]>(`${this.apiUrl}/departments`);
   }
 
-  public getDepartmentById(id: number): Department | undefined {
-    return this.departments.find((department) => {
-      department.id === id
-    });
+  public getDepartmentById(id: number): Observable<Department> {
+    return this.http.get<Department>(`${this.apiUrl}/departments/${id}`);
   }
 
   public deleteDepartment(id: number): Observable<Department> {
